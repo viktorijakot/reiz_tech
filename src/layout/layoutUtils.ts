897bc91898048fb33361d6fba836331e4b08e9e2
Viktorija.K <viktorija.kotaite@gmail.com>
2024-06-06 16:52:52 +0300

    created layout utils

---
+++ src/layout/layoutUtils.ts
@@ -0,0 +1,24 @@
+import { Country } from "../hooks/useCountriesData";
+
+export const sortCountries = (array: Country[], isDescending: boolean) =>
+    array
+      .slice()
+      .sort((a, b) =>
+        isDescending
+          ? b.name.localeCompare(a.name)
+          : a.name.localeCompare(b.name)
+      );
+
+export const rowsPerPageOptions = [5, 10, 20];
+
+export const displayCountriesWithPagination = (
+    data: Country[],
+    currentPage: number,
+    rowsPerPage: number
+  ) => {
+    const startIndex = (currentPage - 1) * rowsPerPage;
+    const endIndex = startIndex + rowsPerPage;
+    const displayedAvailableLunchCards = data.slice(startIndex, endIndex);
+  
+    return displayedAvailableLunchCards;
+  };
